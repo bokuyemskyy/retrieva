cda8cf00f6d4fecaf256c85d493b9e02b53b84ff
Tomashu <80130528+TomashuTTTT7@users.noreply.github.com>
2026-06-07 05:57:17 +0200

    better ui

--- app/app.py
+++ app/app.py
@@ -1,70 +1,104 @@
 from openai import OpenAI
 from core.rag import RAG
-from core.embedding.embedder import EmbedderConfig, OllamaEmbedder
+from core.embedding.embedder import EmbedderConfig, OllamaEmbedder, EmbedderFactory
 from core.ingestion.chunker import Chunker
-from core.ingestion.image_captioner import ImageCaptioner, OllamaVLM
+from core.retrieval.llm import LLMConfig
+from core.ingestion.image_captioner import VLMConfig
+from core.ingestion.image_captioner import ImageCaptioner, OllamaVLM, VLMConfig, VLMFactory
 import streamlit as st
 
-def main():
-    st.set_page_config(page_title="Retrieva", page_icon="🤖")
-
-    llm = OpenAI(
-        api_key="ollama",
-        base_url="http://localhost:11434/v1"
-    )
-
-    chunker = Chunker(
-        chunk_size=512,
-        chunk_overlap=128,
-    )
-
-    image_captioner = ImageCaptioner(
-        vlm=OllamaVLM(model_name="moondream:v2"), 
-        use_ocr=True
-    )
-
-    rag = RAG(
-        storage_dir="./storage",
-        llm_client=llm,
-        llm_model="qwen2.5:3b",  
-        chunker=chunker,
-        image_captioner=image_captioner,
-        postgres_url="postgresql://postgres:postgres@localhost:5432/postgres",
-    )
-
-    embedder_config = OllamaEmbedder(
-        config=EmbedderConfig(
-            provider="ollama",
-            model_name="qllama/bge-large-en-v1.5:q8_0",
+def get_rag() -> RAG:
+    if "rag" not in st.session_state:
+        st.session_state.rag = RAG(
+            storage_dir="./storage",
+            postgres_url="postgresql://postgres:postgres@localhost:5432/postgres",
         )
-    )
 
+        st.session_state.rag.set_llm(LLMConfig(provider="ollama", model_name="qwen2.5:3b"))
+        st.session_state.rag.set_vlm(VLMConfig(provider="ollama", model_name="moondream:v2"))
+    return st.session_state.rag
+
+@st.dialog("Create new workspace")
+def add_new_workspace():
+    name = st.text_input(label="Workspace name", value="default")
+    provider = st.selectbox(label="Provider", options=["ollama", "openai"])
+    model_name = st.text_input(label="Model name", value="nomic-embed-text")
+    base_url = st.text_input(label="Base URL", value="http://localhost:11434")
+    api_key = st.text_input(label="API key")
+
+
+    chunk_size = st.number_input(label="Chunk size", min_value=1, value=2048)
+    chunk_overlap = st.number_input(label="Chunk overlap", min_value=1, value=256)
+
+    if st.button("Create"):
+        get_rag().create_workspace(name, EmbedderConfig(
+            provider,
+            model_name,
+            chunk_size,
+            chunk_overlap,
+            api_key,
+            base_url,
+        ))
+        st.rerun()
+
+
+@st.dialog("Confirm deletion")
+def delete_workspace(name: str):
+    st.write(f"Confirm deletion of {name} workspace?")
+    col1, col2 = st.columns(2)
+    if col1.button("Yes", width="stretch"):
+        get_rag().delete_workspace(name)
+        st.rerun()
+    if col2.button("No", width="stretch"):
+        st.rerun()
+
+@st.dialog("Confirm deletion")
+def delete_all_workspaces():
+    st.write("Confirm deletion of all workspaces?")
+    col1, col2 = st.columns(2)
+    if col1.button("Yes", width="stretch"):
+        get_rag().delete_all_workspaces()
+        st.rerun()
+    if col2.button("No", width="stretch"):
+        st.rerun()
+
+def main():
+    rag = get_rag()
+    st.set_page_config(page_title="Retrieva", page_icon="🤖")
     st.title("Retrieva")
 
-    # rag.create_workspace("test3", embedder_config)
     workspaces = rag.get_workspaces()
     workspace = st.selectbox("Workspace", [workspace.name for workspace in workspaces])
-    rag.select_workspace(workspace)
+    col1, col2, col3 = st.columns(3)
+    if col1.button("Create new workspace", width="stretch"):
+        add_new_workspace()
+    if col2.button("Delete workspace", width="stretch", disabled=workspace is None):
+        delete_workspace(workspace)
+    if col3.button("Delete all workspaces", width="stretch", disabled=len(workspaces) == 0):
+        delete_all_workspaces()
 
-    uploaded_files = st.file_uploader("Upload documents", type=[
-        "pdf",
-        "txt",
-        "md",
-        "png",
-        "jpg",
-        "jpeg",
-        "wav",
-        "mp3",
-    ], accept_multiple_files=True)
-    # for file in uploaded_files:
-        # st.write(file.name)
-        # rag.add_document()
+    if workspace:
+        rag.select_workspace(workspace)
 
-    query = st.chat_input("Ask about something")
-    if query:
-        st.chat_message("user").write(query)
-        result = rag.query(query)
-        st.chat_message("ai").write(result)
+        uploaded_files = st.file_uploader("Upload documents", type=[
+            "pdf",
+            "txt",
+            "md",
+            "png",
+            "jpg",
+            "jpeg",
+            "wav",
+            "mp3",
+        ], accept_multiple_files=True)
+        # for file in uploaded_files:
+            # st.write(file.name)
+            # rag.add_document()
+
+        query = st.chat_input("Ask about something")
+        if query:
+            st.chat_message("user").write(query)
+            result = rag.query(query)
+            st.chat_message("ai").write(result)
 
 
 if __name__ == "__main__":
